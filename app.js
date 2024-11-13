45b21da8e9c168760e3c698fc60aa0f74c9b11ec
Elizabeth Larkin <lizlarkin@highlandtechnology.com>
2024-11-12 17:07:40 -0800

    first commit

--- app.js
+++ app.js
@@ -1,74 +1,74 @@
 
-// var angieButton = document.querySelector("#angieBtn");
 var bonnieButton = document.querySelector("#bonnieBtn");
-var claudiaButton = document.querySelector("#claudiaBtn");
-var dionButton = document.querySelector("#dionBtn");
+// var claudiaButton = document.querySelector("#claudiaBtn");
+// var dionButton = document.querySelector("#dionBtn");
+var darrenButton = document.querySelector("#darrenBtn");
 var garvinButton = document.querySelector("#garvinBtn");
-// var fredButton = document.querySelector("#fredBtn");
 // var jessicaButton = document.querySelector("#jessicaBtn");
 var karenButton = document.querySelector("#karenBtn");
-// var karlaButton = document.querySelector("#karlaBtn");
-var lisaButton = document.querySelector("#lisaBtn");
+var karlaButton = document.querySelector("#karlaBtn");
+// var lisaButton = document.querySelector("#lisaBtn");
 var lizButton = document.querySelector("#lizBtn");
 var miguelButton = document.querySelector("#miguelBtn");
-// var premilaButton = document.querySelector("#premilaBtn");
+var nathanButton = document.querySelector("#nathanBtn");
+var premilaButton = document.querySelector("#premilaBtn");
 var robButton = document.querySelector("#robBtn");
-// var tinaButton = document.querySelector("#tinaBtn");
+var tinaButton = document.querySelector("#tinaBtn");
 var voteButton = document.querySelector("#voteBtn");
 
 let introEl = document.querySelector("#Intro-div");
-// let angieEl = document.querySelector("#Angie-div");
 let bonnieEl = document.querySelector("#Bonnie-div");
-let claudiaEl = document.querySelector("#Claudia-div");
-let dionEl = document.querySelector("#Dion-div");
+// let claudiaEl = document.querySelector("#Claudia-div");
+// let dionEl = document.querySelector("#Dion-div");
+let darrenEl = document.querySelector("#Darren-div");
 let garvinEl = document.querySelector("#Garvin-div");
-// let fredEl = document.querySelector("#Fred-div");
 // let jessicaEl = document.querySelector("#Jessica-div");
 let karenEl = document.querySelector("#Karen-div");
-// let karlaEl = document.querySelector("#Karla-div");
-let lisaEl = document.querySelector("#Lisa-div");
+let karlaEl = document.querySelector("#Karla-div");
+// let lisaEl = document.querySelector("#Lisa-div");
 let lizEl = document.querySelector("#Liz-div");
 let miguelEl = document.querySelector("#Miguel-div");
-// let premilaEl = document.querySelector("#Premila-div");
+let nathanEl = document.querySelector("#Nathan-div");
+let premilaEl = document.querySelector("#Premila-div");
 let robEl = document.querySelector("#Rob-div");
-// let tinaEl = document.querySelector("#Tina-div");
+let tinaEl = document.querySelector("#Tina-div");
 
 function start () {
     introEl.style.display="block";
-    // angieEl.style.display="none";
     bonnieEl.style.display="none";
-    claudiaEl.style.display="none";
-    dionEl.style.display="none";
+    // claudiaEl.style.display="none";
+    // dionEl.style.display="none";
+    darrenEl.style.display="none";
     garvinEl.style.display="none";
-    // fredEl.style.display="none";
     // jessicaEl.style.display="none";
     karenEl.style.display="none";
-    // karlaEl.style.display="none";
-    lisaEl.style.display="none";
+    karlaEl.style.display="none";
+    // lisaEl.style.display="none";
     lizEl.style.display="none";
     miguelEl.style.display="none";
-    // premilaEl.style.display="none";
+    nathanEl.style.display="none";
+    premilaEl.style.display="none";
     robEl.style.display="none";
-    // tinaEl.style.display="none";
+    tinaEl.style.display="none";
 }
 
 function reset () {
     introEl.style.display="none";
-    // angieEl.style.display="none";
     bonnieEl.style.display="none";
-    claudiaEl.style.display="none";
-    dionEl.style.display="none";
+    // claudiaEl.style.display="none";
+    // dionEl.style.display="none";
+    darrenEl.style.display="none";
     garvinEl.style.display="none";
-    // fredEl.style.display="none";
     // jessicaEl.style.display="none";
     karenEl.style.display="none";
-    // karlaEl.style.display="none";
-    lisaEl.style.display="none";
+    karlaEl.style.display="none";
+    // lisaEl.style.display="none";
     lizEl.style.display="none";
     miguelEl.style.display="none";
-    // premilaEl.style.display="none";
+    nathanEl.style.display="none";
+    premilaEl.style.display="none";
     robEl.style.display="none";
-    // tinaEl.style.display="none";
+    tinaEl.style.display="none";
 }
 
 start();
@@ -83,14 +83,19 @@ bonnieButton.addEventListener ("click", function() {
     bonnieEl.style.display="block";
 });
 
-claudiaButton.addEventListener ("click", function() {
-    reset()
-    claudiaEl.style.display="block";
-});
+// claudiaButton.addEventListener ("click", function() {
+//     reset()
+//     claudiaEl.style.display="block";
+// });
 
-dionButton.addEventListener ("click", function() {
+// dionButton.addEventListener ("click", function() {
+//     reset()
+//     dionEl.style.display="block";
+// });
+
+darrenButton.addEventListener ("click", function() {
     reset()
-    dionEl.style.display="block";
+    darrenEl.style.display="block";
 });
 
 garvinButton.addEventListener ("click", function() {
@@ -98,10 +103,6 @@ garvinButton.addEventListener ("click", function() {
     garvinEl.style.display="block";
 });
 
-// fredButton.addEventListener ("click", function() {
-//     reset()
-//     fredEl.style.display="block";
-// });
 
 // jessicaButton.addEventListener ("click", function() {
 //     reset()
@@ -113,15 +114,15 @@ karenButton.addEventListener ("click", function() {
     karenEl.style.display="block";
 });
 
-// karlaButton.addEventListener ("click", function() {
-//     reset()
-//     karlaEl.style.display="block";
-// });
-
-lisaButton.addEventListener ("click", function() {
+karlaButton.addEventListener ("click", function() {
     reset()
-    lisaEl.style.display="block";
+    karlaEl.style.display="block";
 });
+
+// lisaButton.addEventListener ("click", function() {
+//     reset()
+//     lisaEl.style.display="block";
+// });
 
 lizButton.addEventListener ("click", function() {
     reset()
@@ -133,23 +134,28 @@ miguelButton.addEventListener ("click", function() {
     miguelEl.style.display="block";
 });
 
-// premilaButton.addEventListener ("click", function() {
-//     reset()
-//     premilaEl.style.display="block";
-// });
+nathanButton.addEventListener ("click", function() {
+    reset()
+    nathanEl.style.display="block";
+});
+
+premilaButton.addEventListener ("click", function() {
+    reset()
+    premilaEl.style.display="block";
+});
 
 robButton.addEventListener ("click", function() {
     reset()
     robEl.style.display="block";
 });
 
-// tinaButton.addEventListener ("click", function() {
-//     reset()
-//     tinaEl.style.display="block";
-// });
+tinaButton.addEventListener ("click", function() {
+    reset()
+    tinaEl.style.display="block";
+});
 
 voteButton.addEventListener ("click", function() {
-    location.href = "https://www.surveymonkey.com/r/RG2YZRK";
+    location.href = "https://www.surveymonkey.com/r/GYJ8PYC";
 });
 
 
